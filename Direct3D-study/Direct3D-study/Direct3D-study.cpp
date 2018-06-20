99fdf075f9a2b6b265793ed6d0e88fd7a7f278c0
gongluck <1039994845@qq.com>
2018-06-20 15:41:05 +0800

    网格模型

--- Direct3D-study/Direct3D-study/Direct3D-study.cpp
+++ Direct3D-study/Direct3D-study/Direct3D-study.cpp
@@ -13,7 +13,8 @@
 #pragma comment(lib, "d3dx9.lib")
 #pragma comment(lib, "d3d9.lib")
 
-#define TEXUTRE_TEST
+//MATRICES_TEST、LIGHTS_TEST、TEXUTRE_TEST、MESHES_TEST
+#define MESHES_TEST
 
 //自定义定点结构
 #ifdef MATRICES_TEST
@@ -57,8 +58,13 @@ CUSTOMVERTEX g_vertices[100];
 //D3D
 IDirect3D9* g_d3d = nullptr;
 IDirect3DDevice9* g_device = nullptr;
+
 IDirect3DVertexBuffer9* g_vb = nullptr;
 IDirect3DTexture9* g_tx = nullptr;
+
+ID3DXMesh* g_mesh = nullptr;
+D3DMATERIAL9* g_meshmaterial = nullptr;
+LPDIRECT3DTEXTURE9* g_txs = nullptr;
 
 HWND g_hwnd = nullptr;
 
@@ -67,10 +73,7 @@ ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR    lpCmdLine,
-	_In_ int       nCmdShow)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
@@ -101,15 +104,19 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	g_device->SetRenderState(D3DRS_LIGHTING, FALSE); //禁用Direct3D光照
 	g_device->SetRenderState(D3DRS_ZENABLE, TRUE); //打开Z缓冲
 
+#ifndef MESHES_TEST
 	//创建顶点缓冲区
 	g_device->CreateVertexBuffer(sizeof(g_vertices), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_vb, nullptr);
+#endif
 
-	//拷贝数据到顶点缓冲区
-	void* pVertices;
+	void* pVertices = nullptr;
+	DWORD materials = 0;//接收材质数量
+
 #ifdef LIGHTS_TEST
-	for (int i = 0; i<50; i++)
+	//拷贝数据到顶点缓冲区
+	for (int i = 0; i < 50; i++)
 	{
-		FLOAT theta = 2*D3DX_PI/49 *i;
+		FLOAT theta = 2 * D3DX_PI / 49 * i;
 		g_vertices[2 * i + 0].position = D3DXVECTOR3(sinf(theta), -1.0f, cosf(theta));
 		g_vertices[2 * i + 0].normal = D3DXVECTOR3(sinf(theta), 0.0f, cosf(theta));
 		g_vertices[2 * i + 1].position = D3DXVECTOR3(sinf(theta), 1.0f, cosf(theta));
@@ -117,23 +124,47 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	}
 #elif defined TEXUTRE_TEST
 	res = D3DXCreateTextureFromFile(g_device, L"banana.bmp", &g_tx);
-	for (int i = 0; i<50; i++)
+	for (int i = 0; i < 50; i++)
 	{
 		FLOAT theta = 2 * D3DX_PI / 49 * i;
-		g_vertices[2 * i + 0].position = D3DXVECTOR3(sinf(theta), -1.0f, cosf(theta));
+		g_vertices[2 * i + 0].position = D3DXVECTOR3(sinf(theta), -1.0f, -cosf(theta));
 		g_vertices[2 * i + 0].color = 0xffffffff;
 		g_vertices[2 * i + 0].tu = ((FLOAT)i) / 49;
 		g_vertices[2 * i + 0].tv = 1.0f;
 
-		g_vertices[2 * i + 1].position = D3DXVECTOR3(sinf(theta), 1.0f, cosf(theta));
+		g_vertices[2 * i + 1].position = D3DXVECTOR3(sinf(theta), 1.0f, -cosf(theta));
 		g_vertices[2 * i + 1].color = 0xff808080;
 		g_vertices[2 * i + 1].tu = ((FLOAT)i) / 49;
 		g_vertices[2 * i + 1].tv = 0.0f;
 	}
+#elif defined MESHES_TEST
+	ID3DXBuffer* pmb = nullptr;//材质缓冲
+
+	//加载网格模型图片，获取材质
+	res = D3DXLoadMeshFromX(L"Tiger.x", D3DXMESH_SYSTEMMEM, g_device, NULL, &pmb, NULL, &materials, &g_mesh);
+
+	//提取材质和纹理
+	D3DXMATERIAL* pmtrls = (D3DXMATERIAL*)pmb->GetBufferPointer();
+	g_meshmaterial = new D3DMATERIAL9[materials];
+	g_txs = new LPDIRECT3DTEXTURE9[materials];
+	for (int i = 0; i < materials; ++i)
+	{
+		g_meshmaterial[i] = pmtrls[i].MatD3D; //拷贝材质
+		g_meshmaterial[i].Ambient = g_meshmaterial[i].Diffuse;
+		g_txs[i] = nullptr;
+		//用获取到的纹理名字创建纹理
+		res = D3DXCreateTextureFromFileA(g_device, pmtrls[i].pTextureFilename, &g_txs[i]);
+	}
+
+	//释放材质缓冲
+	pmb->Release();
 #endif
+
+#ifndef MESHES_TEST
 	g_vb->Lock(0, 0, &pVertices, 0);
 	memcpy(pVertices, g_vertices, sizeof(g_vertices));
 	g_vb->Unlock();
+#endif
 
 	MSG msg = { 0 };
 	// 主消息循环: 
@@ -150,7 +181,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 				break;
 
 			//清空背景
-			res = g_device->Clear(0, nullptr, D3DCLEAR_TARGET| D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 255, 0), 1.0/*设置Z缓冲的Z初始值*/, 0/*设置模板缓冲的初始值*/);
+			res = g_device->Clear(0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 255, 0), 1.0/*设置Z缓冲的Z初始值*/, 0/*设置模板缓冲的初始值*/);
 
 			//开始“画图”
 			res = g_device->BeginScene();
@@ -189,11 +220,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 #endif
 			//世界坐标系
 			D3DXMATRIX matWorld, *pmatWorld = nullptr;
-			pmatWorld = D3DXMatrixRotationX(&matWorld, timeGetTime() / 500.0f);//绕X轴旋转
+			//pmatWorld = D3DXMatrixRotationX(&matWorld, timeGetTime() / 500.0f);//绕X轴旋转
+			//pmatWorld = D3DXMatrixRotationY(&matWorld, timeGetTime() / 500.0f);//绕Y轴旋转
+			pmatWorld = D3DXMatrixRotationAxis(&matWorld, &D3DXVECTOR3(1,1,0), timeGetTime() / 500.0f);//绕自定义轴旋转
+
 			res = g_device->SetTransform(D3DTS_WORLD, &matWorld);
 
 			//观察坐标系
-			D3DXVECTOR3 vEyePt(0.0f, 0.0f, -5.0f);//眼睛点
+			D3DXVECTOR3 vEyePt(0.0f, 3.0f, -5.0f);//眼睛点
 			D3DXVECTOR3 vLookatPt(0.0f, 0.0f, 0.0f);//观察点
 			D3DXVECTOR3 vUpVec(0.0f, 1.0f, 0.0f);//向上方向
 			D3DXMATRIX matView, *pmatView = nullptr;
@@ -206,15 +240,25 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 				1/*宽高比*/, 1/*近视图平面的Z值*/, 100/*远视图平面的Z值*/);//创建投影矩阵
 			res = g_device->SetTransform(D3DTS_PROJECTION, &matProj);
 
+#ifndef MESHES_TEST
 			//设置渲染源数据
 			res = g_device->SetStreamSource(0, g_vb, 0, sizeof(CUSTOMVERTEX));//内存位置
 			res = g_device->SetFVF(D3DFVF_CUSTOMVERTEX);//格式
+#else
+			for (int i = 0; i < materials; ++i)
+			{
+				g_device->SetMaterial(&g_meshmaterial[i]);//应用材质
+				g_device->SetTexture(0, g_txs[i]);//应用纹理
+				g_mesh->DrawSubset(0);//绘制网格子集
+			}
+#endif
+
 #ifdef MATRICES_TEST
 			res = g_device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);//绘制控制
 #elif defined LIGHTS_TEST
-			res = g_device->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 100);//绘制控制(三角形带)
+			res = g_device->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 98);//绘制控制(三角形带)
 #elif defined TEXUTRE_TEST
-			res = g_device->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 100);//绘制控制(三角形带)
+			res = g_device->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 98);//绘制控制(三角形带)
 #endif		
 			//结束“画图”
 			res = g_device->EndScene();
@@ -224,6 +268,42 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		}
 	}
 
+	//清理
+	if (g_meshmaterial != nullptr)
+	{
+		delete[] g_meshmaterial;
+		g_meshmaterial = nullptr;
+	}
+	if (g_txs != nullptr)
+	{
+		for (int i = 0; i < materials; ++i)
+		{
+			if (g_txs[i] != nullptr)
+			{
+				g_txs[i]->Release();
+				g_txs[i] = nullptr;
+			}
+		}
+		delete[] g_txs;
+		g_txs = nullptr;
+	}
+	if (g_mesh != nullptr)
+	{
+		g_mesh->Release();
+		g_mesh = nullptr;
+	}
+
+
+	if (g_tx != nullptr)
+	{
+		g_tx->Release();
+		g_tx = nullptr;
+	}
+	if (g_vb != nullptr)
+	{
+		g_vb->Release();
+		g_vb = nullptr;
+	}
 	if (g_device != nullptr)
 	{
 		g_device->Release();
